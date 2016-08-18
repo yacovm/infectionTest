33de43a6b8ac6b1fd73c8275a82de20d4e52039b
YACOVM <YACOVM@il.ibm.com>
2016-08-18 11:31:25 +0300

    added byzantine support

--- InfectionTest.java
+++ InfectionTest.java
@@ -1,5 +1,6 @@
 package disseminationTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,17 +8,32 @@ import java.util.Queue;
 
 public class InfectionTest {
 
+	private static List<Integer> byzantinePeers = new ArrayList<Integer>();
+	private static double p = 0.15;
+	private static double h = 1.0;
+	
 	public static void main(String[] args) {
-		for (int n=4; n<7000; n *= 2) {
+		
+		for (int n=128; n<7000; n *= 2) {
+			populateByzantine(n);
 			int notInfectedAttempts = 0;
-			int k = (int) (Math.log(n) + 3);
+			int k = (int) (((int) (Math.log(n) + 3)) * h);
+			
 			for (int i=0; i<1000; i++) {
 				int infectedCount = calculateInfected(n, k);
-				if (infectedCount < n) {
+				if (infectedCount < n - byzantinePeers.size()) {
 					notInfectedAttempts++;
 				}
 			}
-			System.out.println(n + ":" + ((1000 - notInfectedAttempts) / 10) + "%, k: " + k);
+			System.out.println(n + ":" + ((double)(1000 - notInfectedAttempts) / 1000) * 100 + "% success, k: " + k + ", h: " + h);
+		}
+	}
+	
+	private static void populateByzantine(int n) {
+		for (int i=0; i<n; i++) {
+			if (Math.random() < p) {
+				byzantinePeers.add(i);
+			}
 		}
 	}
 	
@@ -47,6 +63,9 @@ public class InfectionTest {
 		while (! nodes2act.isEmpty()) {
 			// get next id of infected node 
 			int p = nodes2act.poll();
+			if (byzantinePeers.contains(p)) {
+				continue;
+			}
 			infectedNodes[p] = true;
 			// disseminate to k random nodes
 			selectRandomPeers(n, k).stream().forEach(q -> {
